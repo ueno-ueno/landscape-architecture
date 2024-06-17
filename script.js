61cb154b1962b5e5e222b099ce911abfcbae7f7d
tomotomoz <tomoko.essvivh@gmail.com>
2024-06-17 18:55:36 +0900

    修正

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ fade.forEach(el => {
 
 y: 20, 
 opacity: 0, 
-duration: 1, 
+duration: 1.5, 
 ease:'power4.Out' },
 
 {
 y: 0, 
 opacity: 1, 
-duration: 1, 
+duration: 1.5, 
 ease: 'power4.out',
 
 scrollTrigger: {
